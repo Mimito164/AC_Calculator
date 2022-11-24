cb1c0a2b06b6ffee4361da8c82eb9697433d5d84
Federico Jaralampidis <fjaralampidis@gmail.com>
2022-11-23 22:21:29 -0300

    Updated and renamed example file

---
+++ Example.py
@@ -0,0 +1,21 @@
+from Calculator import calc
+from ComplexNumer import *
+
+
+ComplexNumer.setOmega(100)
+
+V = VoltageSource("V", 20, 1)
+R = Resistor("R", 220)
+C = Capacitor("C", 100e-3)
+L = Inductor("L", 25e-3)
+
+if __name__ == "__main__":
+    zp = calc.paralel("Zp", L, R, True)
+    zt = calc.sum("Zt", zp, C, True)
+    it = calc.division("It", V, zt, True)
+
+    vc = calc.voltageDivider("Vc", V, C, zp, True)
+    vp = calc.voltageDivider("Vp", V, zp, C, True)
+    ir = calc.division("Ir", vp, R, True)
+    il = calc.division("Ir", vp, L, True)
+    powersV = calc.power("S*", V, it, True)
