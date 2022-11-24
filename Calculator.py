5cf8ddfa8ba631f7d8f0bddb682752c803183505
Federico Jaralampidis <fjaralampidis@gmail.com>
2022-11-23 22:28:26 -0300

    Formated file

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,11 @@
-from ComplexNumer import ComplexNumer
+from ComplexNumer import *
 import re
 
 
 class Calculator(object):
     def __new__(cls):
         if not hasattr(cls, 'instance'):
-            cls.instance = super( Calculator, cls).__new__(cls)
+            cls.instance = super(Calculator, cls).__new__(cls)
         return cls.instance
 
     def sum(self, complexName: str, a: ComplexNumer, b: ComplexNumer,
@@ -46,9 +46,9 @@ class Calculator(object):
         result = ComplexNumer(complexName, a.value/b.value)
         if verbose:
             print(f"{self.toSpaces(result.name)}   {a.name}   {a.toExp()}")
-            
+
             print(f"{result.name} = {self.toHyphen(max(a.name,b.name))} = {self.toHyphen(max(a.toExp(),b.toExp()))} = {result.toExp()}")
-            
+
             print(f"{self.toSpaces(result.name)}   {b.name}   {b.toExp()}")
 
             print("\n\n")
@@ -71,8 +71,8 @@ class Calculator(object):
             d2 = a.toRect() + " + " + b.toRect()
             n3 = self.product("", a, b).toExp()
             d3 = self.sum("", a, b).toExp()
-            w2 = len(max(n2,d2))
-            w3 = len(max(n3,d3))
+            w2 = len(max(n2, d2))
+            w3 = len(max(n3, d3))
             print(
                 f"{self.toSpaces(result.name)}   {n1}   {n2:{w2}}   {n3:{w3}}")
             print(
@@ -82,54 +82,57 @@ class Calculator(object):
             print("\n\n")
         return result
 
-    def voltageDivider(self, complexName: str, v: ComplexNumer,
+    def voltageDivider(self, complexName: str, v: VoltageSource,
                        z1: ComplexNumer, z2: ComplexNumer, verbose=False) -> ComplexNumer:
         if verbose:
-            result = ComplexNumer(complexName, v.value * (z1.value / (z1.value + z2.value)))
+            result = ComplexNumer(complexName, v.value *
+                                  (z1.value / (z1.value + z2.value)))
             d1 = z1.name + " + " + z2.name
             d2 = z1.toRect() + " + " + z2.toRect()
-            d3 = self.sum("",z1,z2).toExp()
+            d3 = self.sum("", z1, z2).toExp()
             n3 = z1.toExp()
             division = self.division("", z1, self.sum("", z1, z2))
             print(
                 f"{self.toSpaces(result.name)}   {self.toSpaces(v.name)}   {z1.name:{len(d1)}}    {self.toSpaces(v.toExp())}  {z1.toExp():{len(d2)}} {self.toSpaces(v.toExp())}     {n3:{len(n3)}}")
-            
+
             print(
                 f"{result.name} = {v.name} * {self.toHyphen(d1)} = {v.toExp()} * {self.toHyphen(d2)} = {v.toExp()} * {self.toHyphen(max(n3,d3))} = {v.toExp()} * {division.toExp()} = {result.toExp()}")
-            
+
             print(
                 f"{self.toSpaces(result.name)}   {self.toSpaces(v.name)}   {d1}    {self.toSpaces(v.toExp())}  {d2} {self.toSpaces(v.toExp())}     {d3}")
             print("\n\n")
         return result
 
-    def currentDivider(self, complexName: str, i: ComplexNumer,
+    def currentDivider(self, complexName: str, i: CurrentSource,
                        z1: ComplexNumer, z2: ComplexNumer, verbose=False) -> ComplexNumer:
-        
-        result = ComplexNumer(complexName, i.value * (z2.value / (z1.value + z2.value)))
-        
+
+        result = ComplexNumer(complexName, i.value *
+                              (z2.value / (z1.value + z2.value)))
+
         if verbose:
             d1 = z1.name + " + " + z2.name
             d2 = z1.toRect() + " + " + z2.toRect()
-            d3 = self.sum("",z1,z2).toExp()
+            d3 = self.sum("", z1, z2).toExp()
             division = self.division("", z2, self.sum("", z1, z2))
             print(
                 f"{self.toSpaces(result.name)}   {self.toSpaces(i.name)}   {z2.name:{len(d1)}}    {self.toSpaces(i.toExp())}  {z2.toExp():{len(d2)}} {self.toSpaces(i.toExp())}     {z2.toExp():{len(d2)}}")
-            
+
             print(
                 f"{result.name} = {i.name} * {self.toHyphen(d1)} = {i.toExp()} * {self.toHyphen(d2)} = {i.toExp()} * {self.toHyphen(d3)} = {i.toExp()} * {division.toExp()} = {result.toExp()}")
-            
+
             print(
                 f"{self.toSpaces(result.name)}   {self.toSpaces(i.name)}   {d1}    {self.toSpaces(i.toExp())}  {d2} {self.toSpaces(i.toExp())}     {d3}")
             print("\n\n")
         return result
 
-    def power(self, complexName: str, v: ComplexNumer, i: ComplexNumer,
+    def power(self, complexName: str, v: VoltageSource, i: CurrentSource,
               verbose=False) -> ComplexNumer:
 
         result = ComplexNumer(complexName, 1/2 * v.value * i.value.conjugate())
         if verbose:
-            iConj = ComplexNumer("",i.value.conjugate())
-            print(f"{result.name} = 1/2 * {v.toExp()} * ~({i.toExp()}) = 1/2 * {v.toExp()} * {iConj.toExp()}")
+            iConj = ComplexNumer("", i.value.conjugate())
+            print(
+                f"{result.name} = 1/2 * {v.toExp()} * ~({i.toExp()}) = 1/2 * {v.toExp()} * {iConj.toExp()}")
             print(f"{result.name} = {result.toExp()}")
             print(f"S* = {result.toExp()}")
             print(f"S = {abs(result.value)}[V.A]")
